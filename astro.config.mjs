55e7f4ea126680a5700bc88624f024943c880c40
Nik Szejna <szejna@gmail.com>
2024-03-12 14:34:47 -0600

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,23 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
+	  output: "server",
+      adapter: cloudflare({
+      runtime: {
+      	mode: "local"
+      }
+  })
+});
+
+
+
+
+// https://astro.build/config
+export default defineConfig({
+
 });
